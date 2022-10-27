d9cf17cbeca2ff5673ed359f7806548c1c4800e8
Christian Brennecke-Räther <c.b.raether@outlook.com>
2022-10-27 21:10:05 +0200

    GUM-11 fix sql schema including redundant PK

--- database/schema.v1.sql
+++ database/schema.v1.sql
@@ -3,6 +3,5 @@ CREATE TABLE album (
   id SERIAL PRIMARY KEY,
   title VARCHAR(128) NOT NULL,
   artist VARCHAR(255) NOT NULL,
-  price DECIMAL(5, 2) NOT NULL,
-  PRIMARY KEY ('id')
+  price DECIMAL(5, 2) NOT NULL
 );
