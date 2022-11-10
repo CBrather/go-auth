1106142627853e45997aed6cb30454e7cb1620ba
Christian Brennecke-Räther <c.b.raether@outlook.com>
2022-11-10 09:04:50 +0100

    GUM-8 split db connection string env var

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
-
-	"github.com/CBrather/go-auth/internal/repositories/album"
 
 	"github.com/CBrather/go-auth/internal/api"
 )
@@ -20,14 +19,10 @@ func main() {
 		log.Printf("Error loading the .env file: %v, will execute with environment as-is", err)
 	}
 
-	db, err := sql.Open("postgres", os.Getenv("DB_CONNECTION_STRING"))
+	db, err := sql.Open("postgres", fmt.Sprintf(
+		"host=%s port=%s dbname=%s user=%s password=%s sslmode=%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_SSLMODE")))
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	_, err = album.ListByArtist(db, "John Coltrane")
-	if err != nil {
-		log.Printf("%v", err)
 	}
 
 	router := gin.Default()
