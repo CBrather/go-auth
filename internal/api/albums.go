43170e2e7ffb2b9cce8108b34770fab5e05c9998
Christian Brennecke-Räther <c.b.raether@outlook.com>
2023-01-02 16:03:48 +0100

    GUM-29 switch logging to zap with json output

--- internal/api/albums.go
+++ internal/api/albums.go
@@ -2,13 +2,13 @@ package api
 
 import (
 	"database/sql"
-	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	adapter "github.com/gwatts/gin-adapter"
 	_ "github.com/lib/pq"
+	"go.uber.org/zap"
 
 	"github.com/CBrather/go-auth/internal/api/middleware"
 	"github.com/CBrather/go-auth/internal/repositories/album"
@@ -28,8 +28,13 @@ func SetupAlbumRoutes(router *gin.Engine, setupDb *sql.DB) {
 func listAlbums(ginCtx *gin.Context) {
 	albums, err := album.List(db)
 	if err != nil {
-		log.Print(err)
+		zap.L().Error("Failed to retrieve the list of albums.", zap.Error(err))
+
+		ginCtx.AbortWithStatus(http.StatusInternalServerError)
+		return
 	}
+
+	zap.L().Info("Successfully retrieved a list of albums to return")
 
 	ginCtx.IndentedJSON(http.StatusOK, albums)
 }
@@ -37,16 +42,16 @@ func listAlbums(ginCtx *gin.Context) {
 func getAlbum(ginCtx *gin.Context) {
 	idString := ginCtx.Params.ByName("id")
 	id, err := strconv.Atoi(idString)
-	id64 := int64(id)
 
 	if err != nil {
 		ginCtx.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid id"})
 		return
 	}
+
+	id64 := int64(id)
 	album, err := album.GetByID(db, id64)
 
 	if err != nil {
-		log.Print(err)
 		ginCtx.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 
 		return
@@ -58,12 +63,12 @@ func getAlbum(ginCtx *gin.Context) {
 func postAlbum(ginCtx *gin.Context) {
 	var newAlbum album.Album
 	if err := ginCtx.BindJSON(&newAlbum); err != nil {
-		log.Printf("POST /album :: Deserializing Reques failed: %v", err)
+		return
 	}
 
 	id, err := album.Add(db, newAlbum)
 	if err != nil {
-		log.Printf("%v", err)
+		ginCtx.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "failed saving the requested album"})
 	}
 
 	ginCtx.IndentedJSON(http.StatusCreated, gin.H{"id": id})
